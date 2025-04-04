8710cd6f03089ed7383d8d5267ea0997ddf96a48
AKSHATcseb <akshatindia2011@ce.du.ac.in>
2025-04-04 13:02:35 +0530

    done

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,15 @@
 const express = require("express") ;
 const router = express.Router() ;
+const ownerModel = require("../models/owners_model");
 
 router.get("/", function (req, res) {
 res.send("hey owner route is working");
 });
+
+if(process.env.NODE_ENV === "development"){
+    router.post("/create", function (req, res) {
+        res.send("hey, create for owner route is working");
+        });
+}
 
 module. exports = router;
