593795c7d43e7925d9d4168f6fbb4ec988d3e155
AKSHATcseb <akshatindia2011@ce.du.ac.in>
2025-04-04 17:52:02 +0530

    user register route done

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,13 @@
 const express = require("express") ;
 const router = express.Router() ;
+const {registerUser} = require("../controllers/authController");
 
 router.get("/", function (req, res) {
 res.send("hey user route is working");
 });
+
+// issme try karna ye feature ke jab tak user saare info. nahi deta tab tak user register naa ho sake
+router.post("/register", registerUser);
+
 
 module. exports = router;
