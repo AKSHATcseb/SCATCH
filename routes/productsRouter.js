188b94b0bad0123369cc96361d55dce451a76ff7
AKSHATcseb <akshatindia2011@ce.du.ac.in>
2025-04-06 18:17:59 +0530

    done

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,31 +1,31 @@
-const express = require("express") ;
-const router = express.Router() ;
+const express = require("express");
+const router = express.Router();
 const upload = require("../config/multer_config");
 const productModel = require("../models/product_model");
 
 router.get("/", function (req, res) {
-res.send("hey product route is working");
+    res.send("hey product route is working");
 });
 
 router.post("/create", upload.single("image"), async function (req, res) {
-try{
-    let {image, name, price, discount, bgcolor, panelcolor, textcolor } = req.body;
+    try {
+        let { image, name, price, discount, bgcolor, panelcolor, textcolor } = req.body;
         let product = await productModel.create({
             image: req.file.buffer,
-            name, 
-            price, 
-            discount, 
-            bgcolor, 
-            panelcolor, 
+            name,
+            price,
+            discount,
+            bgcolor,
+            panelcolor,
             textcolor
         });
 
         req.flash("success", "Product Created Successfully");
         res.redirect("/owners/admin");
-    } catch(err) {
+    } catch (err) {
         res.send("err.message");
     }
-    });
-    
+});
 
-module. exports = router;
+
+module.exports = router;
