d91c5975e7ad474b551c26661b515d79faa8b76f
AKSHATcseb <akshatindia2011@ce.du.ac.in>
2025-04-06 17:33:06 +0530

    done

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
+// package aquiring
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
 const expressSession = require("express-session")
 
+// path aquiring
 const app = express() ;
 const path = require("path");
 const ownersRouter = require("./routes/ownersRouter");
 const productsRouter = require("./routes/productsRouter");
 const usersRouter = require("./routes/usersRouter");
 const indexRouter = require("./routes/index");
-
+const db = require("./config/mongoose_connection");
 require("dotenv").config();
 
 
-
-const db = require("./config/mongoose_connection");
-
+//  Middleware Setup
 app.use(express.json());
 app. use(express.urlencoded({ extended: true }) );
 app.use(cookieParser());
@@ -27,13 +27,20 @@ app.use(
     expressSession({
     resave: false,
     saveUninitialized: false,
-    secret: process.env.EXPRESS_SESSION_SECRET,
+    secret: process.env.JWT_KEY,
     })
 );
 
+// setting up base routes 
 app.use("/owners", ownersRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
+app.use("/", indexRouter);
 
 app.listen(3000);
+
+
+
+
+
 
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const isloggedin = require("../middlewares/isLoggedIn");
+const productModel = require("../models/product_model");
+const isLoggedIn = require("../middlewares/isLoggedIn");
+const userModel = require("../models/user_model");
 
 
 router.get("/", function (req, res) {
     let error = req.flash("error");
+    // res.render("index", {error, loggedin: false});
     res.render("index", {error});
 });
 
-router.get("/shop", isloggedin, function (req, res) {
-    res.render("shop");
+router.get("/shop", isloggedin, async function (req, res) {
+    let products = await productModel.find();
+    let success = req.flash("success");
+    res.render("shop", { products, success });
+});
+
+router.get("/cart", isloggedin, async function (req, res) {
+    // let products = await productModel.find();
+    // let success = req.flash("success");
+    res.render("cart", { products });
+});
+
+router.get("/addtocart/:id", isloggedin, async function (req, res) {
+    let user = await userModel.findOne({email: req.user.email});
+    user.cart.push(req.params.productid);
+    await user.save();
+    req.flash("success" , "added to cart");
+    res.redirect("/shop");
 });
 
 router.get("/logout", isloggedin, function(req, res) {
